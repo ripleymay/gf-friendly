ec398b4a3f2742e62113a32cfb14494b87310dbe
Ripley Mayfield <ripleymay@Ripleys-MacBook-Pro.local>
2021-10-18 12:23:28 -0500

    basic schemas & new restaurant page

--- models/rstrnt.js
+++ models/rstrnt.js
@@ -1,9 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    content: String,
+    satisfied: Boolean,
+    creator: Schema.Types.ObjectId
+});
+
+const menuItemSchema = new Schema({
+    name: String,
+    price: Number,
+    creator: Schema.Types.ObjectId
+});
+
 const rstrntSchema = new Schema({
     name: String,
-    
+    address: String,
+    url: String,
+    phone: String,
+    creator: Schema.Types.ObjectId,
+    comments: [commentSchema],
+    menuItems: [menuItemSchema]
 }, {
     timestamps: true
 });
--- routes/rstrnts.js
+++ routes/rstrnts.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('rstrnts/index');
 });
 
+router.get('/new', function(req, res) {
+  res.render('rstrnts/new')
+});
+
 module.exports = router;
