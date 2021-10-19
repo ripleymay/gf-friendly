ac0dbe8bba3f2a5df3ad8667a90197faf2a8d750
Ripley Mayfield <ripleymay@Ripleys-MBP.attlocal.net>
2021-10-19 13:23:48 -0500

    deletes menu items

--- controllers/menus.js
+++ controllers/menus.js
@@ -1,7 +1,8 @@
 const Rstrnt = require('../models/rstrnt');
 
 module.exports = {
-    create
+    create,
+    delete: deleteMenu
 };
 
 function create(req, res) {
@@ -12,4 +13,13 @@ function create(req, res) {
             res.redirect(`/rstrnts/${rstrnt._id}`);
         });
     });
+}
+
+async function deleteMenu(req, res) {
+    const rstrnt = await Rstrnt.findById(req.params.rstrntId);
+    const menu = rstrnt.menus.id(req.params.menuId);
+    if (!menu.creator.equals(req.user._id)) return res.redirect(`/rstrnts/${rstrnt._id}`);
+    menu.remove();
+    await rstrnt.save();
+    res.redirect(`/rstrnts/${rstrnt._id}`);
 }
--- routes/menus.js
+++ routes/menus.js
@@ -4,5 +4,6 @@ var menusCtrl = require('../controllers/menus');
 const isLoggedIn = require('../config/auth');
 
 router.post('/rstrnts/:id/menus', isLoggedIn, menusCtrl.create);
+router.delete('/rstrnts/:rstrntId/menus/:menuId', menusCtrl.delete);
 
 module.exports = router;
